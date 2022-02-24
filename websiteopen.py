20577898d89b0b8d0032ab0044a04e8da79eb7e7
blue spinny <honerjerome@gmail.com>
2022-02-24 19:16:17 +0100

    v.1.2
    new features added
    - graveyard
    - custom names

--- websiteopen.py
+++ websiteopen.py
@@ -9,33 +9,112 @@ main = Tk()
 
 
 def setup():
+    
+    with open('graveyard.txt' , 'r') as graveyard:
+        adress = graveyard.read()
+    with open('graveyard.txt','w') as graveyard:
+        graveyard.write(adress)
+        b3.configure(text=adress[adress.find("https://") + 8:adress.find(".com")])
+        currentsave.pack()
+        b2.pack()
+        b1.pack()
+        e1.pack()
+        inf.pack()
+        e2.pack()
+        space.pack()
+        inf2.pack()
+        b3.pack()  
+
+        if adress.find("www") > 0:
+            currentsave.configure(text="current website : " + adress[adress.find("https://www") + 12:adress.find(".com")])
+            currentsave.pack()
+            b2.pack()
+            b1.pack()
+            e1.pack()
+            inf.pack()
+            e2.pack()
+            space.pack()
+            inf2.pack()
+            b3.pack()  
+
+
     with open('safe.txt' , 'r') as safe_file:
         adress = safe_file.read()
-        currentsave.configure(text=adress[adress.find("https://") + 8:adress.find(".com")])
-        if adress.find("www") > 0:
-            currentsave.configure(text=adress[adress.find("https://www") + 12:adress.find(".com")])
+        fname_start = adress.find("nS")
+        fname_end = adress.find("nE")
+        if fname_start - fname_end > 2:
+            currentsave.configure(text="current website : " + adress[adress.find("https://") + 8:adress.find(".com")])
+            if adress.find("www") > 0:
+                currentsave.configure(text="current website : " + adress[adress.find("https://www") + 12:adress.find(".com")])
+        else:
+            currentsave.configure(text="current website : " + adress[fname_start + 2:fname_end])
         
 
 def saveadress():
+    with open('safe.txt' , 'r') as safe_file:
+        adress = safe_file.read()
+        fname_start = adress.find("nS")
+        fname_end = adress.find("nE")
+        
+    with open('graveyard.txt','w') as graveyard:
+        graveyard.write(adress)
+        b3.configure(text=adress[adress.find("https://") + 8:adress.find(".com")])
+        currentsave.pack()
+        b2.pack()
+        b1.pack()
+        e1.pack()
+        inf.pack()
+        e2.pack()
+        space.pack()
+        inf2.pack()
+        b3.pack()
+        if adress.find("www") > 0:
+            currentsave.configure(text="current website : " + adress[adress.find("https://www") + 12:adress.find(".com")])
+            currentsave.pack()
+            b2.pack()
+            b1.pack()
+            e1.pack()
+            inf.pack()
+            e2.pack()
+            space.pack()
+            inf2.pack()
+            b3.pack()  
+
     adress = e1.get()
+        
     with open('safe.txt','w') as safe_file:
         safe_file.write("")
-        safe_file.write(e1.get())
-        currentsave.configure(text=adress[adress.find("https://") + 8:adress.find(".com")])
-        if adress.find("www") > 0:
-            currentsave.configure(text=adress[adress.find("https://www") + 12:adress.find(".com")])
+        safe_file.write(e1.get() + "nS" + e2.get() + "nE")
+        if e2.get() == "":
+            currentsave.configure(text="current website : " + adress[adress.find("https://") + 8:adress.find(".com")])
+            if adress.find("www") > 0:
+                currentsave.configure(text="current website : " + adress[adress.find("https://www") + 12:adress.find(".com")])
+        else:
+            currentsave.configure(text ="current website : " +  str(e2.get()))
             
 
 def define_browser():
     with open('safe.txt','r') as safe_file:
         adress = safe_file.read()
     
-    webbrowser.open(adress)
+    webbrowser.open(adress[0:adress.find("nS")])
+def opengraveyard():
+    with open('graveyard.txt','r') as safe_file:
+        adress = safe_file.read()
+    
+    webbrowser.open(adress[0:adress.find("nS")])
     
 b1 = Button(main,text="open",command=define_browser)
+b3 = Button(main,text="close",command = opengraveyard)
 b2 = Button(main,text="safe",command=saveadress)
-e1 = Entry(main)
+e1 = Entry(main,bd=1)
+e2 = Entry(main,bd=2)
 currentsave = Label(main,text="")
+
+inf = Label(main,text="def")
+inf2 = Label(main,text="graveyard")
+
+space = Label(main,text="")
     
 setup()
 
@@ -46,6 +125,8 @@ currentsave.pack()
 b2.pack()
 b1.pack()
 e1.pack()
+inf.pack()
+e2.pack()
 
 
 
